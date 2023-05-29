4f7df20796812781eda03d83a39e74d8c8335798
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-05-29 00:11:23 -0300

    (Feat) Create a request to test the API

--- src/main/java/com/fabiocarlesso/Main.java
+++ src/main/java/com/fabiocarlesso/Main.java
@@ -1,9 +1,36 @@
 package com.fabiocarlesso;
 
-import static java.lang.System.*;
+import okhttp3.*;
+import org.json.JSONObject;
 
 public class Main {
-    public static void main(String[] args) {
-        err.println("Hello world!");
+    
+    private static final String API_URL = "https://api.openai.com/v1/completions";
+    private static final String API_KEY = "YOUR_API_KEY";
+
+    public static void main(String[] args) throws Exception {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        String prompt = "My name is";
+        JSONObject data = new JSONObject();
+        data.put("model", "text-davinci-003");
+        data.put("prompt", prompt);
+        data.put("max_tokens", 4000);
+        data.put("temperature", 1.0);
+
+        RequestBody body = RequestBody.create(data.toString(), mediaType);
+
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(body)
+                .addHeader("Authorization", "Bearer " + API_KEY)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+        System.out.println(responseBody);
     }
 }
